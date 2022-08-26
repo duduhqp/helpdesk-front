e93d2f2d1b53cd80d1dfe370ac64c364c86b278c
duduhqp <duduhqp@gmail.com>
2022-08-26 15:27:41 -0300

    validação de formularios para login

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-nav',

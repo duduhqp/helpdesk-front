215fea9514653bea41e71cae6dd7bab4a65f2203
epeniche <epeniche@iclass.com.br>
2022-10-19 12:45:26 -0300

    rota e componente para chamado-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import {ClienteListComponent} from "./components/cliente/cliente-list/cliente-li
 import {ClienteCreateComponent} from "./components/cliente/cliente-create/cliente-create.component";
 import {ClienteUpdateComponent} from "./components/cliente/cliente-update/cliente-update.component";
 import {ClienteDeleteComponent} from "./components/cliente/cliente-delete/cliente-delete.component";
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 
 @NgModule({
@@ -60,7 +61,8 @@ import {ClienteDeleteComponent} from "./components/cliente/cliente-delete/client
     ClienteListComponent,
     ClienteCreateComponent,
     ClienteUpdateComponent,
-    ClienteDeleteComponent
+    ClienteDeleteComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
